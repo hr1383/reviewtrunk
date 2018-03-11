e9eeb27c474baf3ace35772b2abe9bde4596ec7f
Harsh <hrsht.rastogi13@gmail.com>
2018-03-10 20:03:47 -0800

    change host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #mailer 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'reviewtrunk.com' }
   config.action_mailer.delivery_method = :smtp
   # change to true to allow email to be sent during development
   config.action_mailer.perform_deliveries = true
